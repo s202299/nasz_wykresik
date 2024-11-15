083ae02d1d7813e4b19387f85c6a3a2df9812d86
Natalia Cebulska <s181777@student.pg.edu.pl>
2024-11-15 12:59:54 +0100

    tt

--- Nasz wykres.R
+++ Nasz wykres.R
@@ -2,3 +2,7 @@ library(tidyverse)
 install.packages(tidyverse)
 install.packages("tidyverse")
 library(tidyverse)
+# pobranie danych
+library(ggplot2)
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
+  geom_point()
