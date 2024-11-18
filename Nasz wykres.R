12f4abf90ba71ee9e74c8779bf05265994e05fea
s202299 <s202299@student.pg.edu.pl>
2024-11-18 20:42:35 +0100

    Labels and theme_minimal added

--- Nasz wykres.R
+++ Nasz wykres.R
@@ -5,4 +5,7 @@ library(tidyverse)
 # pobranie danych
 library(ggplot2)
 ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
-  geom_point()
+  geom_point() +
+labs(x = "Engine displacement", y = "Highway mileage") + 
+  theme_minimal()
+
