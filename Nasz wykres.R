99471d9921040abc3afbcf86f717c87cc4e7f17c
Natalia Cebulska <s181777@student.pg.edu.pl>
2024-11-22 12:17:46 +0100

    Zapisanie lokalnych zmian

--- Nasz wykres.R
+++ Nasz wykres.R
@@ -2,7 +2,4 @@ library(tidyverse)
 install.packages(tidyverse)
 install.packages("tidyverse")
 library(tidyverse)
-# pobranie danych
-library(ggplot2)
-ggplot(mpg, aes(x = displ, y = hwy, color = drv)) +
-  geom_point()
+
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # nasz_wykresik
+# pobranie danych
+
+library(ggplot2)
+ggplot(mpg, aes(x = displ, y = hwy, color = drv)) + geom_point()
+  
+  
